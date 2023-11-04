5e3271dbd8840ee1cf33a3b6d9536eb5aa373179
leen-gh <leen.ghaleb.d@gmail.com>
2023-11-05 02:26:53 +0300

    assigment is done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity) {
+    activity? true : activity = "roller-skate"
+    return "This Saturday, I want to " + activity +"!"
+    
+  }
+
+  const mondayWork = function (str) {
+    str? true : str = "go to the office" 
+    return "This Monday, I will " + str + "."
+  };
+  
+  function wrapAdjective(strr ="*") {
+    const innerFunction = function (str) {
+      return "You are " + strr + str + strr +"!"
+    };
+    return innerFunction
+  }
